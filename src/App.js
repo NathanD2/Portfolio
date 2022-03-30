fab32023cd21b68478308fc27465f24c7c87a7bb
Nathan <ndong5@my.bcit.ca>
2022-03-30 12:38:43 -0700

    project data moved from App.js to external file

--- src/App.js
+++ src/App.js
@@ -9,108 +9,15 @@ import About from "./components/About"
 import MainAbout from "./components/MainAbout"
 import MainContactMe from "./components/MainContactMe"
 
+import { HighlightProjectData } from './ProjectData'
+
 function App() {
 
-  // States are immutable. Need to use setTasks to create a new State.
-  const [hightlightProjects, setHightlightProjects] = useState([
-    {
-      id: 0,
-      projectImageName: "https://i.imgur.com/NfZxz6S.jpg",
-      projectTitle: "Web Portfolio",
-      tags: [
-        {tag: 'Javascript', color: 'lightgreen'},
-        {tag: 'React', color: 'lightblue'},
-        {tag: 'Sass', color: 'pink'},
-        {tag: 'CSS', color: 'pink'},
-        {tag: 'HTML5', color: 'lightblue'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    },
-    {
-      id: 1,
-      projectImageName: "https://i.imgur.com/71ur9Al.gif",
-      projectTitle: "Title",
-      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    },
-    {
-      id: 2,
-      projectImageName: "https://i.imgur.com/71ur9Al.gif",
-      projectTitle: "Title",
-      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    }
-  ]);
+  const [hightlightProjects, setHightlightProjects] = useState([]);
 
-  const [projects, setProjects] = useState([
-    {
-      id: 0,
-      projectImageName: "https://i.imgur.com/NfZxz6S.jpg",
-      projectTitle: "Web Portfolio",
-      tags: [
-        {tag: 'Javascript', color: 'lightgreen'},
-        {tag: 'React', color: 'lightblue'},
-        {tag: 'Sass', color: 'pink'},
-        {tag: 'CSS', color: 'pink'},
-        {tag: 'HTML5', color: 'lightblue'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    },
-    {
-      id: 1,
-      projectImageName: "https://i.imgur.com/71ur9Al.gif",
-      projectTitle: "Title",
-      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    },
-    {
-      id: 2,
-      projectImageName: "https://i.imgur.com/71ur9Al.gif",
-      projectTitle: "Title",
-      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    },
-    {
-      id: 3,
-      projectImageName: "https://i.imgur.com/71ur9Al.gif",
-      projectTitle: "Title",
-      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    },
-    {
-      id: 4,
-      projectImageName: "https://i.imgur.com/71ur9Al.gif",
-      projectTitle: "Title",
-      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    },
-    {
-      id: 5,
-      projectImageName: "https://i.imgur.com/71ur9Al.gif",
-      projectTitle: "Title",
-      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    },
-    {
-      id: 6,
-      projectImageName: "https://i.imgur.com/71ur9Al.gif",
-      projectTitle: "Title",
-      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    },
-    {
-      id: 7,
-      projectImageName: "https://i.imgur.com/71ur9Al.gif",
-      projectTitle: "Title",
-      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    },
-    {
-      id: 8,
-      projectImageName: "https://i.imgur.com/71ur9Al.gif",
-      projectTitle: "Title",
-      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
-      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
-    },
-  ]);
+  useEffect(() => {
+    setHightlightProjects(HighlightProjectData);
+  })
 
   return (
     <Router>
@@ -131,7 +38,7 @@ function App() {
             } />
             <Route path='/projects' element={
               <>
-                <Projects projects={projects} />
+                <Projects />
               </>} />
             <Route path='/about' element={
               <>
---
+++ src/ProjectData.js
@@ -0,0 +1,115 @@
+
+const HighlightProjectData = [
+    {
+      id: 0,
+      projectImageName: "https://i.imgur.com/NfZxz6S.jpg",
+      projectTitle: "Web Portfolio",
+      tags: [
+        {tag: 'Javascript', color: 'lightgreen'},
+        {tag: 'React', color: 'lightblue'},
+        {tag: 'Sass', color: 'pink'},
+        {tag: 'CSS', color: 'pink'},
+        {tag: 'HTML5', color: 'lightblue'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    },
+    {
+      id: 1,
+      projectImageName: "https://i.imgur.com/71ur9Al.gif",
+      projectTitle: "Title",
+      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    },
+    {
+      id: 2,
+      projectImageName: "https://i.imgur.com/71ur9Al.gif",
+      projectTitle: "Title",
+      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    }
+  ];
+
+
+const ProjectData = [
+    {
+      id: 0,
+      projectImageName: "https://i.imgur.com/NfZxz6S.jpg",
+      projectTitle: "Web Portfolio",
+      tags: [
+        {tag: 'Javascript', color: 'lightgreen'},
+        {tag: 'React', color: 'lightblue'},
+        {tag: 'Sass', color: 'pink'},
+        {tag: 'CSS', color: 'pink'},
+        {tag: 'HTML5', color: 'lightblue'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    },
+    {
+      id: 1,
+      projectImageName: "https://i.imgur.com/71ur9Al.gif",
+      projectTitle: "Title",
+      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    },
+    {
+      id: 2,
+      projectImageName: "https://i.imgur.com/71ur9Al.gif",
+      projectTitle: "Title",
+      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    },
+    {
+      id: 3,
+      projectImageName: "https://i.imgur.com/71ur9Al.gif",
+      projectTitle: "Title",
+      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    },
+    {
+      id: 4,
+      projectImageName: "https://i.imgur.com/71ur9Al.gif",
+      projectTitle: "Title",
+      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    },
+    {
+      id: 5,
+      projectImageName: "https://i.imgur.com/71ur9Al.gif",
+      projectTitle: "Title",
+      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    },
+    {
+      id: 6,
+      projectImageName: "https://i.imgur.com/71ur9Al.gif",
+      projectTitle: "Title",
+      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    },
+    {
+      id: 7,
+      projectImageName: "https://i.imgur.com/71ur9Al.gif",
+      projectTitle: "Title",
+      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    },
+    {
+      id: 8,
+      projectImageName: "https://i.imgur.com/71ur9Al.gif",
+      projectTitle: "Title",
+      tags: [{tag: 'Javascript', color: 'lightgreen'}, {tag: 'React', color: 'lightblue'}, {tag: 'Javascript', color: 'pink'}],
+      desc: "Excepteur reprehenderit non dolor excepteur. Deserunt ea voluptate deserunt consectetur aliquip nulla commodo. Ex reprehenderit commodo aliqua non qui laboris sit. Voluptate nostrud fugiat deserunt proident ut eiusmod in pariatur ad occaecat Lorem aliqua cillum esse."
+    },
+  ];
+
+// export function HighlightProjectData() {
+//     return highlightProjectData;
+// }
+
+// export function ProjectData() {
+//     return projectData;
+// }
+
+  
+export {
+    HighlightProjectData,
+    ProjectData
+};
